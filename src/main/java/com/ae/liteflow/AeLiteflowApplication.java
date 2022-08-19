4aa9afa22556c4ab971e8d690dea20594128348e
PC-201904141803\Administrator <402106264@qq.com>
2022-08-19 15:20:26 +0800

    提交

---
+++ src/main/java/com/ae/liteflow/AeLiteflowApplication.java
@@ -0,0 +1,13 @@
+package com.ae.liteflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AeLiteflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AeLiteflowApplication.class, args);
+    }
+}
